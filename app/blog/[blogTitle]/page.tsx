48fe82ec407425a55d4a1199ade9d89f2af21b58
Brook Nash <brookdotcn@outlook.com>
2024-02-25 21:16:34 +0000

    fix: improve blog title and date overflow

--- app/blog/[blogTitle]/page.tsx
+++ app/blog/[blogTitle]/page.tsx
@@ -17,13 +17,13 @@ const BlogPage: NextPage<Props> = async ({ params }): Promise<JSX.Element> => {
 
   return (
     <div className="innerPage">
-      <div className="flex justify-between">
-        <h1 className="font-bold text-2xl">{blogByTitle?.title!}</h1>
-        <span className="flex gap-4">
-          <p className="text-sm text-neutral-400">
-            {blogByTitle?.createdAt!.toLocaleDateString()}
-          </p>
-        </span>
+      <div className="flex justify-between gap-4 sm:gap-0 items-center">
+        <h1 className="font-bold text-2xl text-ellipsis break-words">
+          {blogByTitle?.title!}
+        </h1>
+        <p className="text-sm text-neutral-400">
+          {blogByTitle?.createdAt!.toLocaleDateString()}
+        </p>
       </div>
       <hr />
       <Markup rawMarkdown={blogByTitle?.content!} />
