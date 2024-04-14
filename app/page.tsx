4037453d18e5a131d3076051888598a73858fe92
Brook Nash <brookdotcn@outlook.com>
2024-04-14 13:00:55 +0100

    feat: react tagged blogs

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next";
 import { type JSX } from "react";
-import { IoIosTimer } from "react-icons/io";
+import { FaReact } from "react-icons/fa";
+import { TbTimeDuration10 } from "react-icons/tb";
 import Card from "@/components/card";
 import prisma from "@/lib";
 
@@ -10,29 +11,69 @@ const IndexPage: NextPage = async (): Promise<JSX.Element> => {
     orderBy: { createdAt: "desc" },
   });
 
+  const reactBlogs = await prisma.blog.findMany({
+    include: { tags: true },
+    orderBy: { createdAt: "desc" },
+    where: {
+      tags: {
+        some: {
+          name: "react",
+        },
+      },
+    },
+  });
+
   return (
-    <section className="page">
-      <div className="page-header">
-        <IoIosTimer size={24} />
-        <h1 className="page-title">Recent Blogs</h1>
+    <section className="page gap-12">
+      <div className="flex flex-col gap-4">
+        <div className="page-header">
+          <TbTimeDuration10 size={24} />
+          <h1 className="page-title">Recent Blogs</h1>
+        </div>
+
+        <p className="page-subtitle">
+          Be up to date on my latest blogs, left most recent.
+        </p>
+
+        <div className="flex gap-4 overflow-x-auto rounded">
+          {recentBlogs.map((blog) => {
+            return (
+              <Card
+                key={blog.id}
+                title={blog.title}
+                createdAt={blog.createdAt}
+                description={blog.description}
+                tags={blog.tags}
+              />
+            );
+          })}
+        </div>
       </div>
 
-      <p className="page-subtitle">
-        Be up to date on my latest blogs, left most recent.
-      </p>
+      <div className="flex flex-col gap-4">
+        <div className="page-header">
+          <FaReact size={24} />
+          <h1 className="page-title">React</h1>
+        </div>
 
-      <div className="flex gap-4 overflow-x-auto rounded">
-        {recentBlogs.map((blog) => {
-          return (
-            <Card
-              key={blog.id}
-              title={blog.title}
-              createdAt={blog.createdAt}
-              description={blog.description}
-              tags={blog.tags}
-            />
-          );
-        })}
+        <p className="page-subtitle">
+          Blogs containing information on React, one of the most popular tools
+          in web development.
+        </p>
+
+        <div className="flex gap-4 overflow-x-auto rounded">
+          {reactBlogs.map((blog) => {
+            return (
+              <Card
+                key={blog.id}
+                title={blog.title}
+                createdAt={blog.createdAt}
+                description={blog.description}
+                tags={blog.tags}
+              />
+            );
+          })}
+        </div>
       </div>
     </section>
   );
